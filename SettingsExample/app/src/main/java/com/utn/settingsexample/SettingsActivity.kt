2d87f09c50e7c441f439cb935e48c01032c65371
dibuyo <dibuyo@gmail.com>
2021-05-05 18:14:26 -0300

    Ajustes de Splash

--- SettingsExample/app/src/main/java/com/utn/settingsexample/SettingsActivity.kt
+++ SettingsExample/app/src/main/java/com/utn/settingsexample/SettingsActivity.kt
@@ -9,11 +9,14 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
+
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings, SettingsFragment())
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
