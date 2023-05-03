6fb1a572c6cf5185f5cc6ea576a2a6660c91ba94
Martin Rivas <dibuyo@gmail.com>
2023-05-03 13:28:04 -0300

    Add Title & Nav

--- Toolbar2/app/src/main/java/ar/edu/ort/toolbar2/MainActivity.kt
+++ Toolbar2/app/src/main/java/ar/edu/ort/toolbar2/MainActivity.kt
@@ -1,8 +1,12 @@
 package ar.edu.ort.toolbar2
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,4 +16,27 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbarx)
         setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.default_menu, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menu?.findItem(R.id.menu_opt_shared)?.setEnabled(false)
+
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.menu_opt_find) {
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
